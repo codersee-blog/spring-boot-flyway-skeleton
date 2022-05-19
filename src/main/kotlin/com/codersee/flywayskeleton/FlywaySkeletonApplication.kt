ea6b1a84ea962c8e56d841d78ea6a8d30a4ab00a
Piotr Wolak <contact@codersee.com>
2022-05-19 07:32:30 +0200

    Upload

---
+++ src/main/kotlin/com/codersee/flywayskeleton/FlywaySkeletonApplication.kt
@@ -0,0 +1,11 @@
+package com.codersee.flywayskeleton
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlywaySkeletonApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlywaySkeletonApplication>(*args)
+}
---
+++ src/test/kotlin/com/codersee/flywayskeleton/FlywaySkeletonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codersee.flywayskeleton
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlywaySkeletonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
